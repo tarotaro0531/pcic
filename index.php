ed5fbbfb79b923201a71c26344d7221a4d7e0781
tarotaro0531 <45225409+tarotaro0531@users.noreply.github.com>
2018-12-05 13:00:17 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,123 @@
+<html>
+<head>
+<title>
+板橋高中資訊社
+</title>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.6/jquery.min.js"></script>
+ <script type="text/javascript" src="./js/jquery.cycle.lite.js"></script> 
+ <script>
+ function keyFunction() {
+	
+	if ((event.altKey) && (event.keyCode==17))//alt+ctrl-->>網頁跳轉
+		document.location.href="https://www.pornhub.com/";
+}
+document.onkeydown=keyFunction;
+</script>
+  </head>
+  <body style="background-color:#F5F5DC;"></body>
+   <body>         
+	 <nobr> <div style="width:180px;height:120px">
+	 <img src="C:\Users\Cynthia Liao\Downloads\pcshic_icon (1).png" alt="板中資訊社" style="height:120px;width:180px;" >
+	  <a href="http://www.pcsh.ntpc.edu.tw/"title="PCSH"target="_blank"><font face="微軟正黑體" color="#cc33ff" size="5" >本校首頁</font></nobr></a>
+	 </div>
+   </body>
+   <body>
+      <div style="background-color:#99DD00;width:1890px;height:50px;border-style:double;border-color:#00008B;">
+	    <marquee style="behavior=alternate;color:#00008B;"scrollamount="8"><font face="CHILLER" size="6">歡迎進入板橋高中資訊社網頁 WELCOME TO PCIC'S WEBSITE</font></marquee>
+	  </div>
+   </body>
+   <br>
+   <body>
+     <div style="background-color:#CCFF33;width:1890px;height:40px;border-radius: 25px;">
+	  <center>
+		 &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<nobr><font face="微軟正黑體" color="#cc33ff" size="5">
+	       <a href="file:///C:/Users/Cynthia%20Liao/Desktop/pcic%20幹部介紹.html"title="幹部介紹"target="_blank">幹部介紹</a></font></nobr>
+		 &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<nobr><font face="微軟正黑體" color="#cc33ff" size="5">
+		   <a href="file:///C:/Users/Cynthia%20Liao/Desktop/活動介紹.html"title="活動介紹"target="_blank">活動介紹</a></font></nobr>
+		 &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<nobr><font face="微軟正黑體" color="#cc33ff" size="5">
+		   <a href="file:///C:/Users/Cynthia%20Liao/Desktop/分組介紹.html"title="分組介紹"target="_blank">分組介紹</a></font></nobr>
+         &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<nobr><font face="微軟正黑體" color="#cc33ff" size="5">
+		   <a href="file:///C:/Users/Cynthia%20Liao/Desktop/歷年記事.html"title="歷年記事"target="_blank">歷年記事</a></font></nobr>
+	     &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<nobr><font face="微軟正黑體" color="#cc33ff" size="5">
+		   <a href="file:///C:/Users/Cynthia%20Liao/Desktop/相關比賽.html"title="相關比賽及心得技巧"target="_blank">相關比賽及心得技巧</a></font></nobr>
+      </center>
+     </div>
+   </body>
+   <br>
+   <br>
+   <body>
+    <div style="height:30px;width:1890px;border-radius:10px;background-color:#CCFF99;">
+	  <center><font  color="#4B0082" size="4">關於板橋高中資訊社&nbsp&nbspAbout&nbspPCSH's&nbspInfor&nbspClub</font></center>
+  </div>
+  <div>
+	<center>
+	 <nobr><font size="5">你是否也曾有過一個</font></nobr>
+	 <nobr><font size="6" color="#DC143C">駭客夢</font></nobr>
+	 <nobr><font size="5">呢?</font></nobr>
+	 <p><font size="5">你是否也想要發現隱藏在網路之下的暗潮?</font></p>
+	 <p><font size="5">你是否佩服那些遊戲作者&nbsp動動手指頭就能掌握生殺大權?</font></p>
+	 <p><font size="5">你是否羨慕那些輕輕鬆鬆就能考上大學的人?</font></p>
+	 <p><font size="5">又或者</font></p>
+	 <p><font size="5">你在Coding過程中成就滿滿?</font></p>
+	 <p><font size="5">那麼，資訊社是你最好的選擇。</font></p>
+     <p><font size="3">{&nbsp資訊社的好康&nbsp1.我們沒有學長姐制(什喵???)&nbsp2.我們沒有社費((www</font></p>
+	 <p><font size="3">3.沒有程式設計經驗的也可以參加(((驚呆了!我要搶資訊社!&nbsp}</font></p>
+	 <p><font size="5">這麼好得社團，你還在等甚麼?</font></p>
+	 <nobr><font size="6">趕快加入，最酷炫狂暴的</font></nobr>
+	 <nobr><font size="7" color="#DC143C">板中資訊社</font></nobr>
+	  <p><font size="3">補充&nbsp:&nbsp我們在每周一晚上有較高難度的培訓課，
+	  <p><font size="3">每周二的晚上有大學AP程式設計的課程(學習c++語法)
+	</center>
+  </div>
+  <br>
+  <body>
+   <div>
+    <p style="float:left;margin-left:30px;" >資訊社 </p>
+   </div>
+     <center>
+	  <div style="height:450px;width:900px;background-color:#E6E6FA;">
+     </center>
+      </div>
+  </body>
+ <br>
+ <br>
+  <body>
+    <div>
+      <center>
+    <p>想多知道關於板中資訊社的一切嗎?歡迎臉書搜尋"板橋高中資訊社"或INSTAGRAM搜尋"pcshic"</p>
+      <nobr><img src="C:\Users\Cynthia Liao\Downloads\qr-code (1).png" alt="臉書搜尋板橋高中資訊社" style="height:200px;width:200px;"></nobr>
+      &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<nobr><img src="..\" alt="IG搜尋pcshic" style="height:200px;width:200px;"></nobr>
+      </center>
+    </div>
+  </body>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <center><FORM>
+  對此網站的評價：
+  <SELECT NAME="LIKE">
+  <OPTION VALUE="非常滿意">非常滿意
+  <OPTION VALUE="還算滿意">還算滿意
+  <OPTION VALUE="很滿意">很滿意
+  <OPTION VALUE="超級滿意">超級滿意
+  </select>
+  </center></form>
+  <br>
+   <form action="enter.php" method="post"><center>
+   意見及回饋:<br>
+   <textarea name="TALK" cols="100" rows="10"></textarea><br>
+    <input type="submit" value="送出(感謝填寫~)">
+   </center></form>
+   <br>
+   <br>
+   <br>
+   <br>
+   <br>
+   <br>
+   <br>
+   <br>
+ </html>
